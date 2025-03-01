e0c526217bef64dc67d7ab053fb4de918320fcdc
DARK-SHADOW <155474069+DARK-SHADOW-NEW@users.noreply.github.com>
2025-03-02 00:42:30 +0530

    Update tool-request.js

--- plugins/tool-request.js
+++ plugins/tool-request.js
@@ -22,7 +22,7 @@ cmd({
         }
 
         const reportedMessages = {};
-        const devNumber = "923427582273"; // Bot owner's number
+        const devNumber = "94773416478"; // Bot owner's number
         const messageId = m.key.id;
 
         if (reportedMessages[messageId]) {
