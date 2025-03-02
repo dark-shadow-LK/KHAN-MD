c08ffc7b172d54fe627a0895218fb80453167247
DARK-SHADOW <155474069+DARK-SHADOW-NEW@users.noreply.github.com>
2025-03-02 22:02:05 +0530

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -37,7 +37,7 @@ cmd({
                 from,
                 {
                     image: { url: image.src },
-                    caption: `*Results For:* ${query}\n\n> *BY KHAN MD ❤️‍🩹*`
+                    caption: `*Results For:* ${query}\n\n> *BY DARK SHADOW MD ❤️‍🩹*`
                 },
                 { quoted: mek }
             );
