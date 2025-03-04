8fef325cbcb9ce58fdce2d2b12f4eb0677f60d4f
Dark Shadow <155859542+dark-shadow-LK@users.noreply.github.com>
2025-03-04 08:47:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
